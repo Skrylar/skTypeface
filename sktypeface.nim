9381ec57c709a54bdd578657eb5fbd575e740ba1
Joshua Cearley <joshua.cearley@gmail.com>
2014-04-27 01:18:51 -0500

    Wrote common module for importing typefaces.

--- sktypeface.nim
+++ sktypeface.nim
@@ -2,3 +2,17 @@
 import sktypefaceinternal.sktfcommon
 export sktfcommon
 
+## Retrieves a new typeface system based on whatever OS/build
+## circumstances dictate are the default text system.
+proc NewTypefaceSystem*(): RTypefaceSystem
+
+when hostOS == "windows":
+  import sktypefaceinternal.sktfgdi
+  export sktfgdi
+  proc NewTypefaceSystem*(): RTypefaceSystem =
+    result = NewGdiTypefaceSystem()
+
+when hostOS != "windows":
+  import macros
+  error("No typeface backend is present for this build.")
+
